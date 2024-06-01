73c1b349801e3570cacb5abc2ec121a99587bf2a
Gabriel Bueno <gabrielbueno.pgr@gmail.com>
2024-06-01 11:16:20 -0300

    Finalizando o Site

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]')
+    
+    const herosection = document.querySelector('.hero');
+    const hero_height = herosection.clientHeight;
 
+    window.addEventListener('scroll', function(){
+        const posicao_atual = window.scrollY;
+
+        if(posicao_atual < hero_height){
+            oculta_elementos_header();
+        }else{
+            exibe_elementos_header();
+        }
+    })
+
+    //Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,12 +27,23 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
-    for (let i = 0; i <questions.length; i++){
+    //Seção FAQ, accordions
+    for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreoufecharesposta);
     }
 })
 
-function abreoufecharesposta(elemento){
+function oculta_elementos_header(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibe_elementos_header(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreoufecharesposta(elemento) {
     const classe = 'faq__questions__item--is-open';
     const elementopai = elemento.target.parentNode;
 
@@ -28,7 +53,7 @@ function abreoufecharesposta(elemento){
 function remove_botao() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
-    for (let i = 0; i < buttons.length; i++){
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
